2ba0d42515d37be484d0714fc1c6686b46251537
nathaliariascos <riascosnathalia6@gmail.com>
2022-10-28 17:51:40 -0500

    chore(Root): configuración inicial de redux

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import {useDispatch} from 'react-redux'
-import{actionTest} from '../redux/action'
+import React, { useState } from 'react';
+import {useDispatch, useSelector } from 'react-redux'
+import{ changeText } from 'redux/slices'
 
 
-function Home(props) {
+function Home() {
+    const [ text, setText ] = useState('')
+    const textStore = useSelector((state) => state.text.value)
     const dispatch = useDispatch()
+    
     return (
         <div>
-            home
-            <button onClick={dispatch(actionTest())}>Test</button>
+            <h1>{ textStore }</h1>
+            <input type="text" value={text}  onChange={(e) => setText(e.target.value)}/>
+            <button onClick={() => dispatch(changeText(text))}>Change text</button>
         </div>
     );
 }
--- src/helpers/constans.js
+++ src/helpers/constans.js
@@ -1,2 +1,1 @@
-export const ACTION_TEST = 'ACTION_TEST'
 export const API_URL = 'http://localhost:8081'
