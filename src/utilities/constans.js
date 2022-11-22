ab8fc3a2753dfb8e516c4a954762131037e202e6
Rami Grisales <ramirogrisales@gmail.com>
2022-11-22 17:51:16 -0500

    Update constans.js

--- src/utilities/constans.js
+++ src/utilities/constans.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { AiFillHome, AiOutlineHome } from 'react-icons/ai'
 import { BsFileBarGraph, BsFileBarGraphFill } from 'react-icons/bs'
 import { RiShieldUserFill, RiShieldUserLine } from 'react-icons/ri'
@@ -50,56 +49,3 @@ export const adminPages = [
     href: '/serv'
   }
 ]
-=======
-import { AiFillHome, AiOutlineHome } from 'react-icons/ai'
-import { BsFileBarGraph, BsFileBarGraphFill } from 'react-icons/bs'
-import { RiShieldUserFill, RiShieldUserLine } from 'react-icons/ri'
-
-import { Home } from 'pages'
-import { Card } from 'pages/Home/components'
-
-export const SECTIONS = [
-  {
-    title: 'INICIO',
-    path: '/',
-    activeIcon: AiFillHome,
-    inactiveIcon: AiOutlineHome,
-    component: Home
-  },
-  {
-    title: 'PANEL DE ADMIN',
-    path: '/admin',
-    activeIcon: RiShieldUserFill,
-    inactiveIcon: RiShieldUserLine
-  },
-  {
-    title: 'INFORMES',
-    path: '/info',
-    activeIcon: BsFileBarGraphFill,
-    inactiveIcon: BsFileBarGraph,
-    component: Card
-  }
-]
-
-export const filteredSections = SECTIONS.filter((s) => s.path !== '/admin')
-export const PA = SECTIONS.find((s) => s.path === '/admin')
-
-export const adminPages = [
-  {
-    title: 'Estaciones',
-    href: '/stat'
-  },
-  {
-    title: 'Técnicos',
-    href: '/tecn'
-  },
-  {
-    title: 'Productos',
-    href: '/prod'
-  },
-  {
-    title: 'Servicios',
-    href: '/serv'
-  }
-]
->>>>>>> 48008d3ae4976e72cdf6063387e04cb84f4784be
