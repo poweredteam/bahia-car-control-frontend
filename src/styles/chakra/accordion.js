c797bf2de0cbad4d9b226730e010be20251703e4
Rami Grisales <ramirogrisales@gmail.com>
2022-11-22 17:50:32 -0500

    Update accordion.js

--- src/styles/chakra/accordion.js
+++ src/styles/chakra/accordion.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { accordionAnatomy } from '@chakra-ui/anatomy'
 import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
 
@@ -24,30 +23,3 @@ const baseStyle = definePartsStyle({
 })
 
 export const accordionTheme = defineMultiStyleConfig({ baseStyle })
-=======
-import { accordionAnatomy } from '@chakra-ui/anatomy'
-import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
-
-const { definePartsStyle, defineMultiStyleConfig } =
-  createMultiStyleConfigHelpers(accordionAnatomy.keys)
-
-const baseStyle = definePartsStyle({
-  root: {
-    w: '100%'
-  },
-  container: {
-    border: 'none'
-  },
-  button: {
-    w: '100%',
-    _hover: { bg: 'none' },
-    fontSize: '2xl'
-  },
-  panel: {
-    p: 0,
-    bg: 'brand.primaryDarkVariant'
-  }
-})
-
-export const accordionTheme = defineMultiStyleConfig({ baseStyle })
->>>>>>> 48008d3ae4976e72cdf6063387e04cb84f4784be
