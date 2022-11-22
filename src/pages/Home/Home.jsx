46da664eb3fd39746f3e15e45a140c17ec8874e5
Rami Grisales <ramirogrisales@gmail.com>
2022-11-22 17:49:30 -0500

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import { Cards, Card } from './components'
-<<<<<<< HEAD
 import Formcard from './components/Form/Form.home'
 import { getClients } from '../../redux/slices/clients'
 
@@ -7,10 +6,6 @@ function Home() {
   const [text, setText] = useState('')
   const textStore = useSelector((state) => state.text.value)
   const dispatch = useDispatch()
-=======
-
-function Home() {
->>>>>>> 48008d3ae4976e72cdf6063387e04cb84f4784be
   return (
     <div>
       <Formcard/>
