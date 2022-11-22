00e991284cfc65553f71b0862e9edc8e6a611169
Rami Grisales <ramirogrisales@gmail.com>
2022-11-22 17:44:38 -0500

    Update Logo.jsx

--- src/components/SideBar/Logo.jsx
+++ src/components/SideBar/Logo.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HStack, Center, Text, Icon } from '@chakra-ui/react'
 
 export default function Logo() {
@@ -11,17 +10,4 @@ export default function Logo() {
     </HStack>
   )
 }
-=======
-import { HStack, Center, Text, Icon } from '@chakra-ui/react'
 
-export default function Logo() {
-  return (
-    <HStack h="20%" w="full" border="1px">
-      <Center>
-        <Icon src={'../'} alt="" />
-      </Center>
-      <Text>Car Bahia</Text>
-    </HStack>
-  )
-}
->>>>>>> 48008d3ae4976e72cdf6063387e04cb84f4784be
