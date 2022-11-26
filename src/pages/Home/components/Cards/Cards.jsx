037227a657ece940993362d60df79a203b392c5d
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-11-25 21:02:22 -0500

    action cards

--- src/pages/Home/components/Cards/Cards.jsx
+++ src/pages/Home/components/Cards/Cards.jsx
@@ -3,14 +3,20 @@ import { Card } from './index'
 import './Cards.css'
 
 function Cards() {
-  const services = useSelector(state => state.service.service)
+  const services = useSelector((state) => state.service.service)
   return (
-    <div className='card-container'>
-      {
-        services && services.map(service => (
-          <Card key ={ service.placa } license= { service.placa } station = { service.estacion } />
-        ))
-      }
+    <div className="card-container">
+      {services &&
+        services.map((service) => (
+          <Card
+            key={service.placa}
+            license={service.placa}
+            station={service.estacion}
+            date={service.fecha}
+            technician={service.tecnico}
+            data={service.data}
+          />
+        ))}
     </div>
   )
 }
--- src/redux/slices/services/index.js
+++ src/redux/slices/services/index.js
@@ -1,1 +1,2 @@
 export * from './serviceSlice'
+export * from './thunk'
