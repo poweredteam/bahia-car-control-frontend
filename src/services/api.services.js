d923448e487b2609715da5de65356445a1aadca7
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2023-01-06 12:53:25 -0500

    test deploy

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 const { REACT_APP_API } = process.env
 
 export const api = axios.create({
-  baseURL: REACT_APP_API
+  baseURL: 'https://oyster-app-4d968.ondigitalocean.app/'
 })
